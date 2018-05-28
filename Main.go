2184ba36e70dc3ae1aab9e4b6bdfa26a75c25321
LilShadowEX <shadow.wii@hotmail.com>
2018-05-28 00:18:00 -0500

    listing words, still failing to merge to same list

--- Main.go
+++ Main.go
@@ -6,12 +6,23 @@ import (
 	"encoding/csv"
 	"os"
 	"io"
+	"strings"
 )
+type Wordseeker struct {
+	word string
+	recurrence int64
+}
 
 func main() {
-	f, _ := os.Open("c:\\test\\test.csv")
+	path := os.Args[1]
+	f, _ := os.Open(path)
 
-	r := csv.NewReader(bufio.NewReader(f)) 
+	r := csv.NewReader(bufio.NewReader(f))
+	// positive := Wordseeker{}
+	// negative := Wordseeker{}
+	var wordtotal []string
+	var temppos []string
+	var tempneg []string
 	for {
 		record, err := r.Read()
 
@@ -19,10 +30,32 @@ func main() {
 			// valdria la pena un Log
 			break
 		}
-		fmt.Println(record)
-		fmt.Println(len(record))
-		for value := range record	{
-			fmt.Printf(" %v\n", record[value])
+
+		//fmt.Println(record)
+		if "n" == record[4]{
+			fmt.Println("esto fue un no")
+			fmt.Println(record[1])
+			wordtotal = strings.Fields(record[1])
+			tempneg = append(tempneg,wordtotal[])
+			wordtotal = strings.Fields(record[2])
+			tempneg = append(tempneg,wordtotal[])
+			fmt.Println(tempneg)
+			fmt.Println("length : ",len(tempneg))
+			}
+
+		if "y" == record[4]{
+			fmt.Println("esto fue un si")
+			fmt.Println(record[1])
+			wordtotal = strings.Fields(record[1])
+			fmt.Println(wordtotal)
+			fmt.Println("length : ",len(wordtotal))
+
+			//words := strings.fields(record[2])
+			//aggregatedwords = append(aggregatedwords,words)
+			//words := strings.fields(record[3])
+			//aggregatedwords = append(aggregatedwords,words)
 		}
 	}
+	fmt.Println("agregated: ")
+	//fmt.Println(aggregatedwords)
 }
